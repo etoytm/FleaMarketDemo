b8b48fdc24a0113a8c3867bd19eea36e24bce27a
杨召 <2905226519@qq.com>
2020-10-29 17:02:54 +0800

    增加退出登录

--- include/alert.php
+++ include/alert.php
@@ -1,12 +1,20 @@
 <?php
-function alt($msg,$to){
+function alt($msg, $to)
+{
     var_dump($msg);
-    echo  "<script>alert('$msg');location.href='$to'</script>";
+    echo "<script>alert('$msg');location.href='$to'</script>";
 }
-function back(){
+
+/**
+ * 退回上一个页面，不显示任何信息
+ */
+function back()
+{
     echo "<script>history.go(-1)</script>";
 }
-function alt_back($msg){
-    echo  "<script>alert('$msg');</script>";
+
+function alt_back($msg)
+{
+    echo "<script>alert('$msg');</script>";
     back();
 }
