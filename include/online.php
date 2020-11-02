321c78a299e158c82500d0a1c795bec9c8dd46f3
杨召 <2905226519@qq.com>
2020-11-02 15:40:09 +0800

    验证登录状态只验证SESSION的uid字段，不验证其他字段

--- include/online.php
+++ include/online.php
@@ -9,7 +9,8 @@ session_start();
 
 function online()
 {
-    if (isset($_SESSION['uid']) && isset($_SESSION['nick']) && isset($_SESSION['major']) && isset($_SESSION['grade'])) {
+    if (isset($_SESSION['uid'])) {
+//    if (isset($_SESSION['uid']) && isset($_SESSION['nick']) && isset($_SESSION['major']) && isset($_SESSION['grade'])) {
 //        echo $_SESSION['uid'].$_SESSION['nick'].$_SESSION['major'].$_SESSION['grade'];
         return true;
     }
@@ -22,6 +23,6 @@ function offline_alert($msg = '请先登录')
 {
     if (!online()) {
         require_once('alert.php');
-        alt($msg,'./login.html');
+        alt($msg, './login.html');
     }
 }
