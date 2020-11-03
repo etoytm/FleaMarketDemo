f4f72dc29345276d9f1b76a75f0179523bd589b2
杨召 <2905226519@qq.com>
2020-11-03 14:59:08 +0800

    个人信息页v1.0

--- crowdfunding.center/my_info.php
+++ crowdfunding.center/my_info.php
@@ -1,6 +1,9 @@
 <?php
 session_start();
-$uid = $_SESSION['uid'];
+require_once "../controller/userManage.php";
+$arr = getUserArrByUid($_SESSION['uid']);
+$sex = $arr['sex'];
+$grade = $arr['grade'];
 
 ?>
 
@@ -41,177 +44,71 @@ $uid = $_SESSION['uid'];
     </ul>
 </div>
 <div class="my_info_content">
-    <table class="my_info_content_care_table">
-        <tbody>
-        <tr>
-            <td width="90" align="right" class="color555">用户名：</td>
-            <td class="color555"><?php echo $uid ?></td>
-        </tr>
-        <tr>
-            <td align="right" class="color555">手手机号码：</td>
-            <td class="color555">18609515574<span class="color959595 margin_left10 font_size12">友好提示：网站前台不会显示您手机号，用于运营人员替您办理投融资手续</span>
-            </td>
-        </tr>
-        <tr>
-            <td align="right" class="color555">真实姓名：</td>
-            <td class="color555"><input class="my_info_content_care_table_text" name="" type="text">
-                <span class="color959595 margin_left10 font_size12">友好提示：网站前台不会显示您手机号，用于运营人员替您办理投融资手续</span></td>
-        </tr>
-        <tr>
-            <td align="right" class="color555">性别：</td>
-            <td class="color555"><label>
-                    <input type="radio" name="sex" id="" value="未知" checked>
-                    未知 </label>
-                <label>
-                    <input type="radio" name="sex" id="" value="男士">
-                    男士 </label>
-                <label class="radio-inline">
-                    <input type="radio" name="sex" id="" value="女士">
-                    女士 </label></td>
-        </tr>
-        <tr>
-            <td align="right" class="color555">邮箱：</td>
-            <td class="color555"><input class="my_info_content_care_table_text" name="" type="text">
-                <span class="color959595 margin_left10 font_size12">友好提示：请填写常用邮箱，您忘记密码时可用邮箱找回，有新项目动态时会发邮件给您</span></td>
-        </tr>
-        <tr>
-            <td align="right" class="color555">身份证号：</td>
-            <td class="color555"><input class="my_info_content_care_table_text" name="" type="text">
-                <span class="colorCA1E37 margin_left10 font_size12">保存后不能修改，请确认无误后再保存</span></td>
-        </tr>
-        <tr>
-            <td align="right" class="color555">&nbsp;</td>
-            <td class="color555"><span class="color959595 margin_left10 font_size12">友情提示：网站不会显示您的身份信息，在有限合伙企业设立时会用到您的身份信息</span>
-            </td>
-        </tr>
-        <tr>
-            <td align="right" class="color555">个人名片：</td>
-            <td class="color555"><img id="cardimg" width="215" height="110"
-                                      src="../img/member_center/carddefault.jpg"></img>
-                <input class="my_info_content_care_table_file" name="" type="file"></td>
-        </tr>
-        <tr>
-            <td align="right" class="color555">&nbsp;</td>
-            <td class="color555"><span
-                        class="color959595 margin_left10 font_size12">友情提示：便于投融双方了解到您的相关信息，从而促进项目尽快达成</span></td>
-        </tr>
-        <tr>
-            <td align="right" class="color555">关注行业：</td>
-            <td class="color555"><label>
-                    <input name="concernIndustry" type="checkbox" value="生活消费"/>
-                    生活消费</label>
-                <label>
-                    <input name="concernIndustry" type="checkbox" value="TMT"/>
-                    TMT </label>
-                <label>
-                    <input name="concernIndustry" type="checkbox" value="互联网金融"/>
-                    互联网金融 </label>
-                <label>
-                    <input name="concernIndustry" type="checkbox" value="房产"/>
-                    房产</label>
-                <label>
-                    <input name="concernIndustry" type="checkbox" value="社区"/>
-                    社区</label>
-                <label>
-                    <input name="concernIndustry" type="checkbox" value="城市基础设施"/>
-                    城市基础设施</label></td>
-        </tr>
-        <tr>
-            <td align="right" class="color555">年薪：</td>
-            <td class="color555"><select name="">
-                    <option value="0">请选择</option>
-                    <option value="1">1万-10万</option>
-                    <option value="2">10万-20万</option>
-                    <option value="3">20万-50万</option>
-                    <option value="4">50万以上</option>
-                </select>
-                <span class="color959595 margin_left10 font_size12">请您如实填写</span></td>
-        </tr>
-        <tr>
-            <td align="right" class="color555">投资额度：</td>
-            <td class="color555">最小投资金额
-                <select name="">
-                    <option value="0">请选择</option>
-                    <option value="1">1千</option>
-                    <option value="1">2千</option>
-                    <option value="1">3千</option>
-                    <option value="1">4千</option>
-                    <option value="1">5千</option>
-                    <option value="1">6千</option>
-                    <option value="1">7千</option>
-                    <option value="1">8千</option>
-                    <option value="1">9千</option>
-                </select>
-                最大投资金额
-                <select name="">
-                    <option value="0">请选择</option>
-                    <option value="1">1万</option>
-                    <option value="1">2万</option>
-                    <option value="1">3万</option>
-                    <option value="1">4万</option>
-                    <option value="1">5万</option>
-                    <option value="1">6万</option>
-                    <option value="1">7万</option>
-                    <option value="1">8万</option>
-                    <option value="1">9万</option>
-                    <option value="1">10万</option>
-                    <option value="1">20万</option>
-                    <option value="1">30万</option>
-                    <option value="1">40万</option>
-                    <option value="1">50万</option>
-                    <option value="1">60万</option>
-                    <option value="1">70万</option>
-                    <option value="1">80万</option>
-                    <option value="1">90万</option>
-                    <option value="1">100万以上</option>
-                </select>
-                <span class="color959595 margin_left10 font_size12">请您如实填写</span><span
-                        class="colorCA1E37 margin_left10 font_size12">友好提示:投资有风险,入股需谨慎!</span></td>
-        </tr>
-        <tr>
-            <td align="right" class="color555">所在城市：</td>
-            <td class="color555">
-                <div id="my_city">
-                    <select class="prov">
+    <form method="post" action="../controller/changeUserInfo.php">
+        <table class="my_info_content_care_table">
+            <tbody>
+            <tr>
+                <td width="90" align="right" class="color555">用户名：</td>
+                <td class="color555"><?php echo $_SESSION['uid'] ?></td>
+            </tr>
+            <tr>
+                <td align="right" class="color555">QQ：</td>
+                <td class="color555">
+                    <label>
+                        <input class="my_info_content_care_table_text" type="text" name="qq"
+                               value="<?php echo $arr['qq'] == null ? '暂未绑定QQ！' : $arr['qq'] ?>">
+                    </label>
+                </td>
+            </tr>
+            <tr>
+                <td align="right" class="color555">昵称：</td>
+                <td class="color555">
+                    <label>
+                        <input class="my_info_content_care_table_text" type="text" name="nick"
+                               value="<?php echo $arr['qq'] == null ? '暂未设置昵称！' : $arr['nick'] ?>">
+                    </label>
+                </td>
+            </tr>
+            <tr>
+                <td align="right" class="color555">性别：</td>
+                <td class="color555">
+                    <label> <input type="radio" name="sex" id="" value="1"
+                            <?php if ($sex == 1) echo 'checked' ?>>男</label>
+                    <label class="radio-inline"> <input type="radio" name="sex"
+                                                        value="0" <?php if ($sex == 0) echo 'checked' ?>>女</label>
+                </td>
+            </tr>
+            <tr>
+                <td align="right" class="color555">学生证号：</td>
+                <td class="color555">
+                    <?php echo $arr['school_number'] == null ? "暂未认证！<a href='#' onclick='listClick(3)'>立即认证>></a>" : $arr['school_number'] ?>
+                </td>
+            </tr>
+            <tr>
+                <td align="right" class="color555">年级：</td>
+                <td class="color555">
+                    <select name="grade">
+                        <option value="1" <?php if ($grade == 1) echo 'selected' ?>>大一</option>
+                        <option value="2" <?php if ($grade == 2) echo 'selected' ?>>大二</option>
+                        <option value="3" <?php if ($grade == 3) echo 'selected' ?>>大三</option>
+                        <option value="4" <?php if ($grade == 4) echo 'selected' ?>>大四</option>
+                        <option value="5" <?php if ($grade == 5) echo 'selected' ?>>保密</option>
                     </select>
-                    <select class="city" disabled="disabled">
-                    </select>
-                    <select class="dist" disabled="disabled">
-                    </select>
-                    <span class="color959595 margin_left10 font_size12">友好提示：我们能更好的为您融资做出更多针对性投融需求服务</span></div>
-            </td>
-        </tr>
-        <tr>
-            <td align="right" class="color555">通讯地址：</td>
-            <td class="color555"><input class="my_info_content_care_table_text" name="" type="text">
-                <span class="color959595 margin_left10 font_size12">请您如实填写</span></td>
-        </tr>
-        <tr>
-            <td align="right" class="color555">&nbsp;</td>
-            <td class="color555"><span class="color959595 margin_left10 font_size12">友好提示：网站前台页面不会显示您的地址，用于某经融公司众筹平台运营管理人员替您办理入股工商变更手续及快递接收</span>
-            </td>
-        </tr>
-        <tr>
-            <td align="right" class="color555">推荐人姓名：</td>
-            <td class="color555"><input class="my_info_content_care_table_text" name="" type="text">
-                <span class="color959595 margin_left10 font_size12">请您如实填写</span></td>
-        </tr>
-        <tr>
-            <td align="right" class="color555">推荐人电话：</td>
-            <td class="color555"><input class="my_info_content_care_table_text" name="" type="text">
-                <span class="color959595 margin_left10 font_size12">请您如实填写</span></td>
-        </tr>
-        <tr>
-            <td align="right" class="color555">验证码：</td>
-            <td class="color555"><input class="my_info_content_care_table_text" name="" type="text">
-                <span class="color959595 margin_left10 font_size12">请输入验证码</span></td>
-        </tr>
-        <tr>
-            <td align="center" class="color555" colspan="2"><input class="my_info_content_care_table_submit" name=""
-                                                                   type="submit" value="保    存"></td>
-        </tr>
-        </tbody>
-    </table>
+                    <span class="color959595 margin_left10 font_size12">请您如实填写并及时更新</span></td>
+            </tr>
+            <tr>
+                <td align="right" class="color555">&nbsp;</td>
+                <td class="color555">
+                    <span class="color959595 margin_left10 font_size12">友好提示：***************</span>
+                </td>
+            </tr>
+            <tr>
+                <td align="center" class="color555" colspan="2">
+                    <input class="my_info_content_care_table_submit" name="" type="submit" value="保     存"></td>
+            </tr>
+            </tbody>
+        </table>
+    </form>
 </div>
 
 <!-- 结束 -->
