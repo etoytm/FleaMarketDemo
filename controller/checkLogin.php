9c8cb2034dca68f0b048046bbde3ae15bb2990a7
杨召 <2905226519@qq.com>
2020-11-13 13:17:28 +0800

    修复登录异常问题

--- controller/checkLogin.php
+++ controller/checkLogin.php
@@ -1,4 +1,5 @@
 <?php
+session_start();
 $arr = array(
     "nameRepetition" => null,
     "passwordError" => null,
@@ -12,7 +13,9 @@ require_once '../class/DB.php';
 $db = new DB();
 $re = $db->query($sql);
 
-if ($re->num_rows == 1)
+if ($re->num_rows == 1) {
     $arr['loginSuccess'] = true;
+    $_SESSION['uid'] = $_POST['uid'];
+}
 
 die(json_encode($arr));
